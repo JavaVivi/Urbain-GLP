3aeefaa8a1965d37885a2a837ada58b5e68bc03b
Dartron François <35767615+dartronfl@users.noreply.github.com>
2018-02-26 08:03:17 +0100

    Update Gui_test.java

--- urban_simulator/src/board/Gui_test.java
+++ urban_simulator/src/board/Gui_test.java
@@ -1,12 +1,14 @@
 package board;
 
+import java.awt.BorderLayout;
 import java.awt.Color;
-
+import java.awt.Component;
+import java.awt.FlowLayout;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
 
-
+import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
@@ -14,9 +16,10 @@ import javax.swing.JPanel;
 import javax.swing.JProgressBar;
 import javax.swing.JTabbedPane;
 
+import data.Grid;
 
 /**
- * @author François-Laurent
+ * @author FranÃ§ois-Laurent
  *
  */
 
@@ -26,31 +29,36 @@ public class Gui_test extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 1L;
-	protected JPanel sectionGauche;
-	protected JPanel sectionMap;
-	protected JPanel sectionDroite;
-	protected JPanel sectionHeure;
-	protected JPanel sectionPersonnage;
-	protected JPanel sectionInfos;
-	protected JPanel liste_besoins;
-	protected JPanel liste_actions;
+	protected JPanel mapSection;
+	protected JPanel rightSection;
+	protected JPanel hourSection;
+	protected JPanel particularSection;
+	protected JPanel infoSection;
+	protected JPanel list_need;
+	protected JPanel list_actions;
+	protected JPanel list_buttonAction;
+	protected JPanel action;
 	
 	protected JButton personnage_1;
 	protected JButton personnage_2;
 	protected JButton personnage_3;
+	protected JButton addaction;
+	protected JButton delaction;
+	
 	
 	protected JLabel chronometre = new JLabel("Heure : ");
 	protected JLabel map = new JLabel("Map :");
 	protected JLabel infos_personnage = new JLabel ("Information :");
 	protected JLabel name = new JLabel("Nom :");
-	protected JLabel besoins = new JLabel ("Besoins :");
-	protected JLabel energie = new JLabel ("Energie :");
-	protected JLabel sante = new JLabel ("Santé :");
-	protected JLabel hygiene = new JLabel ("Hygiène :");
+	protected JLabel needs = new JLabel ("Besoins :");
+	protected JLabel energy = new JLabel ("Energie :");
+	protected JLabel health = new JLabel ("SantÃ© :");
+	protected JLabel hygiene = new JLabel ("HygiÃ¨ne :");
 	protected JLabel humeur = new JLabel ("Humeur");
-	protected JLabel bienetre = new JLabel ("Bien-Être");
+	protected JLabel bienetre = new JLabel ("Bien-ÃŠtre");
 	
 	protected JProgressBar barenergie, barsante, barhygiene, barhum;
+	protected Grid mapGrid;
 	
 	private JTabbedPane tab;
 	
@@ -60,88 +68,89 @@ public class Gui_test extends JFrame {
 	public Gui_test(String titre) {
 		
 		
-		initSectionGauche();
 		initSectionMap();
 		initSectionDroite();
 		
-		this.setLayout(new GridLayout(1,3));
+		this.setLayout(new BorderLayout());
 		
-		this.getContentPane().add(sectionGauche);
-		this.getContentPane().add(sectionMap);
-		this.getContentPane().add(sectionDroite);
+		this.getContentPane().add(mapSection, BorderLayout.CENTER);
+		this.getContentPane().add(rightSection, BorderLayout.EAST);
 		
 		setDefaultCloseOperation(EXIT_ON_CLOSE);
 		setSize(1200, 600);
 		setResizable(false);
 		setVisible(true);
-		
-		
 	}
 	
+	/**
+	 * initialisation de la map 
+	 */
 	
-	public void initSectionGauche () {
+	public void initSectionMap() {
 		
-		sectionGauche = new JPanel();
-		sectionHeure = new JPanel();
-		sectionPersonnage = new JPanel();
+		mapGrid= new Grid();
+		mapSection = new JPanel();
 		
-		sectionGauche.setLayout(new GridLayout(2,1));
-		sectionPersonnage.setLayout(new GridLayout(3,1));
 		
-		sectionHeure.setSize(100, 100);
-		sectionPersonnage.setSize(100, 100);
+		mapSection.add(mapGrid);
+		mapSection.setBackground(Color.black);
+		mapSection.setSize(500, 500);
+	}
+	
+	/**
+	 * ajout des parties relatives aux personnages + heure 
+	 */
+
+	public void initSectionDroite() {
+		
+		rightSection = new JPanel();
+		hourSection = new JPanel();
+		particularSection = new JPanel();
+		particularSection.setLayout(new FlowLayout());
+		rightSection.setLayout(new GridLayout (4, 1));
+		initInfos();
+		initSectionBesoin();
+		
+		//sectionDroite.add(infos_personnage);
+		
 		
 		personnage_1 = new JButton("Perso1");
 		personnage_2 = new JButton("Perso2");
 		personnage_3 = new JButton("Perso3");
 		
-		sectionHeure.add(chronometre);
+		personnage_1.setSize(100, 30);
+		personnage_2.setSize(100, 30);
+		personnage_3.setSize(100, 30);
 		
-		sectionPersonnage.add(personnage_1);
-		sectionPersonnage.add(personnage_2);
-		sectionPersonnage.add(personnage_3);
 		
-		sectionGauche.add(sectionHeure);
-		sectionGauche.add(sectionPersonnage);
-		sectionGauche.setSize(100, 500);
-
+		
+		hourSection.add(chronometre);
+		
+		particularSection.add(personnage_1);
+		particularSection.add(personnage_2);
+		particularSection.add(personnage_3);
+		
+		rightSection.add(hourSection, BorderLayout.NORTH);
+		rightSection.add(infoSection);
+		rightSection.add(tab);
+		rightSection.add(particularSection, BorderLayout.CENTER);
+		rightSection.setSize(100,500);
 	}
 	
-	
-	public void initSectionMap() {
-		
-		sectionMap = new JPanel();
-		//sectionMap.add(map);
-		sectionMap.setBackground(Color.BLACK);
-		sectionMap.setSize(500, 500);
-	}
-	
-
-	public void initSectionDroite() {
-		
-		sectionDroite = new JPanel();
-		
-		sectionDroite.setLayout(new GridLayout (3, 1));
-		initInfos();
-		initSectionBesoin();
-		
-		//sectionDroite.add(infos_personnage);
-		sectionDroite.add(sectionInfos);
-		sectionDroite.add(tab);
-		sectionDroite.setSize(100,500);
-		
-	}
+	/**
+	 * 
+	 */
 	
 	public void initInfos() {
 		
-		sectionInfos = new JPanel();
-		sectionInfos.setLayout(new GridBagLayout());
+		infoSection = new JPanel();
+		infoSection.setLayout(new GridBagLayout());
 		
 		GridBagConstraints b = new GridBagConstraints();
 		
 		b.gridx = 0;
 		b.gridy = 0;
-		sectionInfos.add(name, b);
+		infoSection.add(name, b);
 		
 		/*b.gridx = 1;
 		b.gridy = 0;
@@ -151,7 +160,7 @@ public class Gui_test extends JFrame {
 		
 		b.gridx = 0;
 		b.gridy = 1;
-		sectionInfos.add(bienetre, b);
+		infoSection.add(bienetre, b);
 		
 		/*b.gridx = 1;
 		b.gridy = 1;
@@ -160,15 +169,20 @@ public class Gui_test extends JFrame {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	
 	public void initSectionBesoin() {
 		
-		liste_besoins = new JPanel();
-		liste_actions = new JPanel();
+		list_need = new JPanel();
+		list_actions = new JPanel();
+		list_buttonAction = new JPanel();
+		action = new JPanel();
 		
 		int en, hu, hy,sa;
 		
-		en = 90;
+		en = 100;
 		hu = 70;
 		hy = 50;
 		sa = 60;
@@ -180,59 +194,73 @@ public class Gui_test extends JFrame {
 		
 		tab = new JTabbedPane();
 
-		liste_besoins.setLayout(new GridBagLayout());
+		list_actions.setLayout(new GridLayout());
+		list_need.setLayout(new GridBagLayout());
+		list_buttonAction.setLayout(new FlowLayout());
 		
 		GridBagConstraints c = new GridBagConstraints();
 		
 		c.gridx = 0;
 		c.gridy = 0;
-		liste_besoins.add(energie, c);
+		list_need.add(energy, c);
 		
 		c.gridx = 0;
 		c.gridy = 1;
-		liste_besoins.add(sante, c);
+		list_need.add(health, c);
 		
 		c.gridx = 0;
 		c.gridy = 2;
-		liste_besoins.add(hygiene, c);
+		list_need.add(hygiene, c);
 		
 		c.gridx = 0;
 		c.gridy = 3;
-		liste_besoins.add(humeur, c);
+		list_need.add(humeur, c);
 		
 		//ajout des barres
 		
 		barenergie.setValue(en);
 		c.gridx = 1;
 		c.gridy = 0;
-		liste_besoins.add(barenergie, c);
+		list_need.add(barenergie, c);
 		
 		barsante.setValue(sa);
 		c.gridx = 1;
 		c.gridy = 1;
-		liste_besoins.add(barsante, c);
+		list_need.add(barsante, c);
 		
 		barhygiene.setValue(hy);
 		c.gridx = 1;
 		c.gridy = 2;
-		liste_besoins.add(barhygiene, c);
+		list_need.add(barhygiene, c);
 		
 		barhum.setValue(hu);
 		c.gridx = 1;
 		c.gridy = 3;
-		liste_besoins.add(barhum, c);
+		list_need.add(barhum, c);
 		
 		
-		tab.addTab("Besoin", liste_besoins);
-		tab.addTab("Actions", liste_actions);
+		tab.addTab("Besoin", list_need);
+		
+		addaction = new JButton("Ajouter");
+		delaction = new JButton("Supprimer");
+		
+		list_buttonAction.add(addaction);
+		list_buttonAction.add(delaction);
+		list_actions.add(list_buttonAction);
 		
 		
+		
+		
+		tab.addTab("Actions", list_actions);
 		
 		
 	}
 	
 	
-	
+	/**
+	 * 
+	 * @param args
+	 */
 	
 	public static void main(String[] args) 
 	{
