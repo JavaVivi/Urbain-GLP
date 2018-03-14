c963bb870063ffe5ff964123136049dfafc1cc64
rkane443 <35426047+rkane443@users.noreply.github.com>
2018-03-14 16:03:05 +0100

    Update Simulator.java

--- urban_simulator/src/board/Simulator.java
+++ urban_simulator/src/board/Simulator.java
@@ -1,6 +1,125 @@
 package board;
 
-public class Simulator 
-{
+import java.awt.Graphics;
+import java.awt.image.BufferStrategy;
 
+import assets.AssetSample;
+import assets.Assets;
+import assets.Display;
+import assets.ImageLoader;
+import states.SimulatorState;
+import states.State;
+
+
+public class Simulator implements Runnable {
+
+	private Display display;
+	public int width, height;
+	public String title;
+	
+	private boolean running = false;
+	private Thread thread;
+	
+	private BufferStrategy bs;
+	private BufferStrategy test;
+	private Graphics g;
+	
+	//States
+	private SimulatorState gameState;
+	private State menuState;
+	
+	public Simulator(String title, int width, int height){
+		this.width = width;
+		this.height = height;
+		this.title = title;
+	}
+	
+	private void init(){
+		display = new Display(title, width, height);
+		AssetSample.init();
+		
+	}
+	
+	private void update(){
+		if(State.getState() != null)
+			State.getState().update();
+	}
+	
+	private void render(){
+		bs = display.getCanvas().getBufferStrategy();
+		if(bs == null){
+			display.getCanvas().createBufferStrategy(3);
+			return;
+		}
+		g = bs.getDrawGraphics();
+		//Clear Screen
+		g.clearRect(0, 0, width, height);
+		//Draw Here!
+		
+		if(State.getState() != null)
+			State.getState().render(g);
+		
+		//End Drawing!
+		bs.show();
+		g.dispose();
+	}
+	/**
+	 * this is the tmporary game loop
+	 */
+	public void run(){
+		
+		this.init();
+		
+		int fps = 60;
+		double timePerTick = 1000000000 / fps;
+		double delta = 0;
+		long now;
+		long lastTime = System.nanoTime();
+		long timer = 0;
+		int ticks = 0;
+		
+		while(running){
+			now = System.nanoTime();
+			delta += (now - lastTime) / timePerTick;
+			timer += now - lastTime;
+			lastTime = now;
+			
+			if(delta >= 1){
+				update();
+				render();
+				ticks++;
+				delta--;
+			}
+			
+			if(timer >= 1000000000){
+				System.out.println("Ticks and Frames: " + ticks);
+				ticks = 0;
+				timer = 0;
+			}
+		}
+		
+		stop();
+		
+	}
+	
+	public synchronized void start(){
+		if(running)
+			return;
+		running = true;
+		thread = new Thread(this);
+		thread.start();
+	}
+	
+	public synchronized void stop(){
+		if(!running)
+			return;
+		running = false;
+		try {
+			thread.join();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	
 }
